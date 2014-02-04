7e045d98e45d3dfe9dc5e14ee60e56bb4d68bfa8
Jani Luostarinen <jani.luostarinen@nosto.com>
2014-02-04 11:29:20 +0200

    Use play version in base_url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['play']['dir'] = "/opt/play"
 default['play']['version'] = "1.2.6"
 default['play']['checksum'] = "1c48944fdc7975a5a0a09d9166cef4e0aa63c5b07d52f5fa683a70a4387e6c05"
-default['play']['base_url'] = "http://downloads.typesafe.com/play"
+default['play']['base_url'] = "http://downloads.typesafe.com/play/#{node['play']['version']}"
 default['play']['user'] = "play"
 default['play']['group'] = "play"
 default['play']['home'] = "/srv/play"
