fc3cfaf3fa758863dc9d3e35d7c1fc073eb09959
Jani Luostarinen <jani.luostarinen@nosto.com>
2014-02-05 15:02:44 +0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jani.luostarinen@nosto.com"
 license          "Apache 2.0"
 description      "Installs Play Framework"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 recipe "play::default", "Installs play from binary"
 recipe "play::source", "Installs play from source"
